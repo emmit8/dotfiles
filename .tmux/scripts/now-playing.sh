8f971ce43735049aff10fbdbbe427624c211112c
Roman Belikin <roman.sstu@gmail.com>
2015-09-22 16:14:26 +0300

    Update readme

--- .tmux/scripts/now-playing.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-osascript -e 'tell application "iTunes" to if player state is playing then "♪ " & artist of current track & " - " & name of current track'
-
-
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dotfiles
 emmit's dotfiles
+
+### 1. Requirements
+
+1. Ruby
+2. [psutil](https://github.com/giampaolo/psutil)
