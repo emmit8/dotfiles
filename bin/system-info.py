5e91ab72580538fb1757aee3a8783a8cd0e03c08
Roman Belikin <roman.sstu@gmail.com>
2015-09-25 00:47:42 +0300

    Adjust update interval

--- bin/system-info.py
+++ bin/system-info.py
@@ -10,14 +10,37 @@ Author: Roman Belikin roman[dot]sstu[at]gmail.com
 import os
 import sys
 import psutil
+import time
+
+
+def up_down_rate(interface = 'en0'):
+
+    def net():
+       n = psutil.net_io_counters(pernic=True)[interface]
+       return (n.bytes_sent, n.bytes_recv)
+
+    nst = net()
+    dst = time.time()
+
+    time.sleep(1)
+
+    ul, dl = [(now - last) / (time.time() - dst) / 1000.0
+            for now, last in zip(net(), nst)]
+
+    return (psutil.net_if_addrs()[interface][0].address, ul, dl) 
 
 def info():
+
     mem = psutil.virtual_memory()
-    return "mem >> %s/%sMB cpu >> %s%% " % (
-        str(int(mem.used / 1024 / 1024)),
-        str(int(mem.total / 1024 / 1024)),
-        psutil.cpu_percent(interval=0.1),
-    )
+    hdd = psutil.disk_usage('/')
+    cpu = psutil.cpu_percent(interval=0.1)
+
+    return "inet >> %(net)s hdd >> %(hdd)sGB mem >> %(mem)s cpu >> %(cpu)s%% " % {
+            'net' : 'ip: {0} ul: {1:.0f} kB/s / dl: {2:.0f} kB/s'.format(*up_down_rate()),
+            'hdd' : str(int(hdd.free / 1024 / 1024 / 1024)),
+            'mem' : '{0}/{1}MB'.format(str(int(mem.used / 1024 / 1024)), str(int(mem.total / 1024 / 1024))),
+            'cpu' : str(cpu) 
+    }
 
 def main():
     try:
